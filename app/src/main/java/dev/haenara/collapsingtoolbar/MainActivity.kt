22617962f7795476b78abc866c953ba93054e46b
HaenaraShin <hamster12345@gmail.com>
2020-08-31 16:37:47 +0900

    add basic appbar example

--- app/src/main/java/dev/haenara/collapsingtoolbar/MainActivity.kt
+++ app/src/main/java/dev/haenara/collapsingtoolbar/MainActivity.kt
@@ -1,11 +1,26 @@
 package dev.haenara.collapsingtoolbar
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.widget.FrameLayout
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+
+
+
+        fab.setOnClickListener {
+            Snackbar.make(it, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
     }
+
 }
